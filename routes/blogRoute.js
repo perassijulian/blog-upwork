ab5536d82ec881e332c570c7462d695de1d6cd4a
Julian <perassijulian@gmail.com>
2022-04-30 12:32:35 -0300

    move controllers

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const { getBlog, createBlog } = require('../controllers/blog.js');
 
 const blogRoute = express.Router();
 
-blogRoute.get('/', getBlog)
-blogRoute.post('/', createBlog)
+blogRoute.get('/', async (req, res, next) => {
+    try {
+        const blogPosts = await Blog.find();
+
+        res.status(200).json(blogPosts)
+
+    } catch (err) {
+        res.status(404).json({message: err.message})
+    }
+})
+
+blogRoute.post('/', async (req, res, next) => {
+    const newPost = new Blog(req.body);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+
+    } catch (err) {
+        res.status(409).json({message: err.message})
+    }
+})
 
 module.exports = blogRoute;
