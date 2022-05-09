3911e38276c899cb16c82cde465e8609df03d696
Julian <perassijulian@gmail.com>
2022-05-09 15:00:33 -0300

    heroku deployment

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+dotenv.config();
 const mongoose = require("mongoose");
+const morgan = require('morgan');
+const path = require("path");
+
+//importing routes
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const path = require("path");
 
-dotenv.config();
-app.use(express.json());
-
+//DB connection
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -21,11 +23,24 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+//middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+//routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
+//heroku settings
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+//backend listener
+app.listen(process.env.PORT || "5000", () => {
   console.log("Backend is running.");
 });
