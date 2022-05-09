2e1b36f951bc40fbb17905d0ce760503ed0331e9
Julian <perassijulian@gmail.com>
2022-05-09 13:49:43 -0300

    page responsive

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
-import "./home.css";
+import "./home.scss";
 import axios from "axios";
 import { useLocation } from "react-router";
 
